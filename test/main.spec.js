c490af43cfc3f72323b3db49c5ffc150c65cb34a
Welinton <welinton@realizati.com.br>
2019-02-08 11:43:51 -0200

    Refactoring with ES6 (Babel)

--- test/main.spec.js
+++ test/main.spec.js
@@ -1,64 +1,60 @@
-const { expect } = require('chai')
-const calc = require('../src/main')
+import { expect } from 'chai'
+import { sum, sub, mult, div } from '../src/main'
 
 describe('Calc', () => {
   //smoke tests - basic tests
   describe('Smoke tests ', () => {
 
-    it('should exist the calc lib', () => {
-      expect(calc).to.exist
-    })
-
     it('should exist the method `sum`', () => {
-      expect(calc.sum).to.exist
-      expect(calc.sum).to.be.a('function')
+      expect(sum).to.exist
+      expect(sum).to.be.a('function')
     })
 
 
     it('should exist the method `sub`', () => {
-      expect(calc.sub).to.exist
-      expect(calc.sub).to.be.a('function')
+      expect(sub).to.exist
+      expect(sub).to.be.a('function')
     })
 
 
     it('should exist the method `mult`', () => {
-      expect(calc.mult).to.exist
-      expect(calc.mult).to.be.a('function')
+      expect(mult).to.exist
+      expect(mult).to.be.a('function')
     })
 
 
     it('should exist the method `div`', () => {
-      expect(calc.div).to.exist
-      expect(calc.div).to.be.a('function')
+      expect(div).to.exist
+      expect(div).to.be.a('function')
     })
 
   })
 
   describe('Sum', () => {
-    it('should return 4 when `calc.sum(2,2)`', () => {
-      expect(calc.sum(2,2)).to.be.equal(4)
+    it('should return 4 when `sum(2,2)`', () => {
+      expect(sum(2,2)).to.be.equal(4)
     })
   })
 
   describe('Sub', () => {
-    it('should return 4 when `calc.sub(6,2)`', () => {
-      expect(calc.sub(6,2)).to.be.equal(4)
+    it('should return 4 when `sub(6,2)`', () => {
+      expect(sub(6,2)).to.be.equal(4)
     })
   })
   
   describe('Mult', () => {
-    it('should return 4 when `calc.mult(2,2)`', () => {
-      expect(calc.mult(2,2)).to.be.equal(4)
+    it('should return 4 when `mult(2,2)`', () => {
+      expect(mult(2,2)).to.be.equal(4)
     })
   })
 
   describe('Div', () => {
-    it('should return 4 when `calc.div(8,2)`', () => {
-      expect(calc.div(8,2)).to.be.equal(4)
+    it('should return 4 when `div(8,2)`', () => {
+      expect(div(8,2)).to.be.equal(4)
     })
     
     it('should return `Can not divide by zero`, when divide by 0', () => {
-      expect(calc.div(4,0)).to.be.equal('Can not divide by zero')
+      expect(div(4,0)).to.be.equal('Can not divide by zero')
     })
   })
 
